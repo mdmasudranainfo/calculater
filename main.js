5e708161b2f1b240d7e6c4e6eca3d1bb68b6f106
masud <mdmasude7@gmail.com>
2021-11-20 18:59:06 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function myf(x){
+    calculate.value+=x;
+
+};
+function calcu(){
+    calculate.value = eval(calculate.value );
+    
+};
+function clearAll(){
+    calculate.value= null;
+
+};
+
+
+
+
